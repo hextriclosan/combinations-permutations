95cbbfeb3b351a6333011d6b94f9f0365869e1e3
Igor Rudenko <iostream64@gmail.com>
2020-02-14 16:57:52 +0200

    Calculate number of possible sums - coins problem

---
+++ count_sums.hpp
@@ -0,0 +1,28 @@
+
+#pragma once
+
+long get_sums_count(int n, std::vector<long> c)
+{
+    std::vector<long> numways(n + 1); // numways[x] means # ways to get sum x
+    numways[0] = 1; // init base case n=0
+
+    // go thru coins 1-by-1 to build up numways[] dynamically
+    // just need to consider cases where sum j>=c[i]
+    for (int i = 0; i < c.size(); i++)
+    {
+        for (int j = c[i]; j <= n; j++)
+        {
+            // find numways to get sum j given value c[i]
+            // it consists of those found earlier plus
+            // new ones.
+            // E.g. if c[]=1,2,3... and c[i]=3,j=5,
+            //      new ones will now include '3' with
+            //      numways[2] = 2, that is:
+            //      '3' with '2', '3' with '1'+'1'
+            numways[j] += numways[j - c[i]];
+        }
+    }
+
+    return numways[n];
+}
+
--- main.cpp
+++ main.cpp
@@ -2,8 +2,14 @@
 
 #include "print_unique.hpp"
 
+#include "count_sums.hpp"
+
+#include <iostream>
+
 int main()
 {
     print_unique({1, 2, 3, 4}, 3);
+
+    std::cout << get_sums_count(5, {1,2}) << '\n';
 
 }
